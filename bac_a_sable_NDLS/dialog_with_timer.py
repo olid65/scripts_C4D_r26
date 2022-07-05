717369614428e9896a1a45c413b4173e2f72a69d
Olivier Donzé <olivier.donze@hesge.ch>
2022-07-05 07:14:33 +0200

    Bac à sable suite

--- bac_a_sable_NDLS/dialog_with_timer.py
+++ bac_a_sable_NDLS/dialog_with_timer.py
@@ -12,7 +12,7 @@ NAME_TAG_GRADIENT = 'gradient'
 class DlgBbox(c4d.gui.GeDialog):
 
 
-    TIME_INTERVAL = 1000 #milliseconds
+    TIME_INTERVAL = 10000 #milliseconds
 
     TXT_BTON_TIMER_ON = "stop process"
     TXT_BTON_TIMER_OFF = "start process"
@@ -20,6 +20,9 @@ class DlgBbox(c4d.gui.GeDialog):
     ID_BTON_TEST = 1000
 
     timer = False
+
+    def __init__(self,sandbox):
+        self.sandbox = sandbox
 
     def CreateLayout(self):
 
@@ -42,19 +45,56 @@ class DlgBbox(c4d.gui.GeDialog):
         return True
 
     def Timer(self,msg):
-        print('test')
-        
+        self.sandbox.update()
+        c4d.EventAdd()
+        c4d.CallCommand(12163) # Render View
+
 class Sandbox(object):
-    def __init__(self,depthmap_obj,tag_alts,tag_gradient):
+
+    FN_ASCII = '/Users/olivierdonze/switchdrive/Mandats/Nuit_de_la_science/Programmation_bac_a_sable/depthmap.asc'
+    FN_TREES = '/Users/olivierdonze/switchdrive/Mandats/Nuit_de_la_science/Programmation_bac_a_sable/tags.json'
+
+    SCALE_ALT = 200
+
+
+    def __init__(self,depthmap_obj,tag_alts,tag_gradient,doc):
         self.depthmap_obj = depthmap_obj
         self.tag_alts = tag_alts
         self.tag_gradient = tag_gradient
-        
+        self.doc = doc
 
-def main() -> None:
+    def update(self):
+        self.update_terrain()
+
+    def update_terrain(self):
+        pts = self.depthmap_obj.GetAllPoints()
+        alts = self.get_alts_from_ascii()
+        alts_initial = self.tag_alts.GetAllHighlevelData()
+        gradient_values = self.tag_gradient.GetAllHighlevelData()
+
+        for pt,alt,alt_initial,grad_val in zip(pts,alts,alts_initial,gradient_values):
+            pt.y =alt*grad_val
+
+        self.depthmap_obj.SetAllPoints(pts)
+        self.depthmap_obj.Message(c4d.MSG_UPDATE)
+
+
+    def get_alts_from_ascii(self,header_size = 6):
+        """returns a list of heights multiplied by the scale factor
+           from an asc file of type Esri grid ascii (with 6 lines header)"""
+        alts = []
+        with open(self.FN_ASCII) as f:
+            for i,line in enumerate(f):
+                #we don't need the header (6 lines)
+                if i>=header_size:
+                    alts += [float(alt)*self.SCALE_ALT for alt in line.split()]
+        return alts
+
+
+def get_sandbox(doc) -> Sandbox:
     depthmap_obj = doc.SearchObject(NAME_OBJ_DEPTHMAP)
-    
-    #TODO : error
+
+    #TODO : gestion erreurs
     if not depthmap_obj:
         print('No depthmap object')
         return
@@ -67,10 +107,12 @@ def main() -> None:
         if tag.CheckType(c4d.Tvertexmap):
             if tag.GetName()==NAME_TAG_GRADIENT:
                 tag_gradient = tag
-    #TODO : error
+
+    #TODO : gestion erreurs
     if not tag_alts or not tag_gradient:
         print('manque un tag')
-    
+
+    return Sandbox(depthmap_obj,tag_alts,tag_gradient,doc)
 """
 def state():
     # Defines the state of the command in a menu. Similar to CommandData.GetState.
@@ -78,7 +120,7 @@ def state():
 """
 
 if __name__ == '__main__':
-    main()
-    #dlg = DlgBbox()
-    #dlg.Open(c4d.DLG_TYPE_ASYNC)
-    
+    #main()
+    sandbox = get_sandbox(doc)
+    dlg = DlgBbox(sandbox)
+    dlg.Open(c4d.DLG_TYPE_ASYNC)
