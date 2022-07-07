9819e2ccfffce23c5f1ded9beb013b340a13b395
Olivier Donzé <olivier.donze@hesge.ch>
2022-07-07 12:58:48 +0200

    SandBox suite

--- bac_a_sable_NDLS/dialog_with_timer.py
+++ bac_a_sable_NDLS/dialog_with_timer.py
@@ -1,16 +1,22 @@
 from typing import Optional
 import c4d
+import json
+import random
+import math
 
 doc: c4d.documents.BaseDocument  # The active document
 op: Optional[c4d.BaseObject]  # The active object, None if unselected
 
 NAME_OBJ_DEPTHMAP = 'depthmap'
 NAME_TAG_ALTS = 'altitudes_initiales'
-NAME_TAG_GRADIENT = 'gradient'
 
+NAME_OBJ_TREES = 'arbres'
+NAME_SOURCE_TREES = 'source_arbres'
+
+NB_PX_X = 588
+NB_PX_Z = 432
 
 class DlgBbox(c4d.gui.GeDialog):
-
 
     TIME_INTERVAL = 10000 #milliseconds
 
@@ -54,30 +60,124 @@ class Sandbox(object):
     FN_ASCII = '/Users/olivierdonze/switchdrive/Mandats/Nuit_de_la_science/Programmation_bac_a_sable/depthmap.asc'
     FN_TREES = '/Users/olivierdonze/switchdrive/Mandats/Nuit_de_la_science/Programmation_bac_a_sable/tags.json'
 
-    SCALE_ALT = 200
+    SCALE_ALT = 100
+    SCALE_TREES = 10
+
+    NB_PX_GRADIENT = 100 #nombre de pixel dans les marges
+    SMOOTH = True #True si on veut lisser les marges des bords
 
 
-    def __init__(self,depthmap_obj,tag_alts,tag_gradient,doc):
-        self.depthmap_obj = depthmap_obj
-        self.tag_alts = tag_alts
-        self.tag_gradient = tag_gradient
+    def __init__(self,doc):
+
+        #TODO : gestion des erreurs !
         self.doc = doc
+
+        #depthmap object and tags
+        self.depthmap_obj = self.doc.SearchObject(NAME_OBJ_DEPTHMAP)
+        self.tag_alts = None
+        for tag in self.depthmap_obj.GetTags():
+            if tag.CheckType(c4d.Tvertexmap):
+                if tag.GetName()==NAME_TAG_ALTS:
+                    self.tag_alts = tag
+        #trees
+        self.trees_objs = self.doc.SearchObject(NAME_OBJ_TREES)
+        srce_parent = self.doc.SearchObject(NAME_SOURCE_TREES)
+        #dico pour stocker les sources avec l'id en étiquette et la liste des objets (variantes) en clé
+        self.trees_srce = {int(child.GetName()):child.GetChildren() for child in srce_parent.GetChildren()}
+
 
     def update(self):
         self.update_terrain()
+        self.update_trees()
 
     def update_terrain(self):
         pts = self.depthmap_obj.GetAllPoints()
         alts = self.get_alts_from_ascii()
         alts_initial = self.tag_alts.GetAllHighlevelData()
-        gradient_values = self.tag_gradient.GetAllHighlevelData()
 
-        for pt,alt,alt_initial,grad_val in zip(pts,alts,alts_initial,gradient_values):
-            pt.y =alt*grad_val
+        #pour l'instant alt initial n'est pas utilisée -> pour terrain existant
+        for i,(pt,alt,alt_initial) in enumerate(zip(pts,alts,alts_initial)):
+            #calcul du facteur dans les bords
+            id_row = int(i / NB_PX_X)
+            id_col = i % NB_PX_X
+            factor = 1
+            if self.NB_PX_GRADIENT:
+                x1 = id_col/ self.NB_PX_GRADIENT
+                x2 = (NB_PX_X-1-id_col)/self.NB_PX_GRADIENT
+                z1 = id_row/ self.NB_PX_GRADIENT
+                z2 = (NB_PX_Z-1-id_row)/self.NB_PX_GRADIENT
+                val = min([x1,x2,z1,z2])
+                if self.SMOOTH:
+                    factor = c4d.utils.Smoothstep(0, 1, val)
+                else:
+                    factor = c4d.utils.Clamp( 0, 1,val)
+
+            pt.y =alt*factor
 
         self.depthmap_obj.SetAllPoints(pts)
         self.depthmap_obj.Message(c4d.MSG_UPDATE)
 
+    def update_trees(self):
+        data = self.get_trees_from_json()
+        #si l'objet n'existe pas on le crée
+        if not self.trees_objs:
+            self.trees_objs = c4d.BaseObject(c4d.Onull)
+            self.trees_objs.SetName(NAME_OBJ_TREES)
+            self.doc.InsertObject(self.trees_objs)
+        #sinon on efface les arbres existants
+        else:
+            for o in self.trees_objs.GetChildren():
+                o.Remove()
+
+        for tag in data['tags']:
+
+            inst = c4d.BaseObject(c4d.Oinstance)
+
+            #look at camera tag insertion
+            tag_look = c4d.BaseTag(c4d.Tlookatcamera)
+            inst.InsertTag(tag_look)
+            priority = c4d.PriorityData()
+            priority.SetPriorityValue(c4d.PRIORITYVALUE_MODE,c4d.CYCLE_ANIMATION)
+            tag_look[c4d.EXPRESSION_PRIORITY] = priority
+
+            inst[c4d.INSTANCEOBJECT_RENDERINSTANCE_MODE] = c4d.INSTANCEOBJECT_RENDERINSTANCE_MODE_SINGLEINSTANCE
+
+            inst[c4d.INSTANCEOBJECT_LINK] = random.choice(self.trees_srce[tag['tag_id']])
+
+            #pos = c4d.Vector(tag['position']['x']*FACT_ECHELLE,0,tag['position']['y']*FACT_ECHELLE)
+
+            #ATTENTION j'ai inversé le x et le y parce qu'il que sur les valeurs test
+            #il y a des valeurs en y de 440 qui est plus grand que 432 ! (à demander à Alexis)
+            #si les valeurs partent de zéro enlever les deux -1
+
+            id_pt = (tag['position']['x'])*NB_PX_X + tag['position']['y']
+            pos = c4d.Vector(self.depthmap_obj.GetPoint(id_pt)* self.depthmap_obj.GetMg())
+
+            inst.SetAbsPos(pos)
+            inst.SetAbsScale(c4d.Vector(self.SCALE_TREES))
+
+            #angle  = c4d.utils.DegToRad(tag['rotation'])
+            #inst.SetAbsRot(c4d.Vector(angle,0,0))
+
+            #Je n'arrive pas à rafraichir la vue pour que le tag lookAtCamera fonctionne
+            #j'ai été obligé de bricoler ça
+            self.look_at_camera(inst)
+
+            #nom avec
+            inst.SetName(tag['tag_id'])
+            inst.InsertUnderLast(self.trees_objs)
+            #inst.Message(c4d.MSG_UPDATE)
+
+    def look_at_camera(self,obj):
+        bd = doc.GetRenderBaseDraw()
+        cp = bd.GetSceneCamera(doc)
+        local = cp.GetMg().off * (~(obj.GetUpMg() * obj.GetFrozenMln())) - obj.GetRelPos()
+
+        hpb = c4d.utils.VectorToHPB(local)
+        hpb.z = obj.GetRelRot().z
+        hpb.x += math.pi
+        hpb.y = -hpb.y
+        obj.SetRelRot(hpb)
 
     def get_alts_from_ascii(self,header_size = 6):
         """returns a list of heights multiplied by the scale factor
@@ -87,32 +187,13 @@ class Sandbox(object):
             for i,line in enumerate(f):
                 #we don't need the header (6 lines)
                 if i>=header_size:
-                    alts += [float(alt)*self.SCALE_ALT for alt in line.split()]
+                    alts += [(float(alt)-0.5)*2*self.SCALE_ALT for alt in line.split()]
         return alts
 
-
-def get_sandbox(doc) -> Sandbox:
-    depthmap_obj = doc.SearchObject(NAME_OBJ_DEPTHMAP)
-
-    #TODO : gestion erreurs
-    if not depthmap_obj:
-        print('No depthmap object')
-        return
-    tag_alts = None
-    tag_gradient = None
-    for tag in depthmap_obj.GetTags():
-        if tag.CheckType(c4d.Tvertexmap):
-            if tag.GetName()==NAME_TAG_ALTS:
-                tag_alts = tag
-        if tag.CheckType(c4d.Tvertexmap):
-            if tag.GetName()==NAME_TAG_GRADIENT:
-                tag_gradient = tag
-
-    #TODO : gestion erreurs
-    if not tag_alts or not tag_gradient:
-        print('manque un tag')
-
-    return Sandbox(depthmap_obj,tag_alts,tag_gradient,doc)
+    def get_trees_from_json(self):
+        with open(self.FN_TREES)as f:
+            data = json.loads(f.read())
+        return data
 """
 def state():
     # Defines the state of the command in a menu. Similar to CommandData.GetState.
@@ -121,6 +202,6 @@ def state():
 
 if __name__ == '__main__':
     #main()
-    sandbox = get_sandbox(doc)
+    sandbox = Sandbox(doc)
     dlg = DlgBbox(sandbox)
     dlg.Open(c4d.DLG_TYPE_ASYNC)
--- bac_a_sable_NDLS/import_trees_json.py
+++ bac_a_sable_NDLS/import_trees_json.py
@@ -6,31 +6,61 @@ import random
 doc: c4d.documents.BaseDocument  # The active document
 op: Optional[c4d.BaseObject]  # The active object, None if unselected
 
-FACT_ECHELLE = 1
+FACT_ECHELLE = 10
+
+NB_PX_X = 588
+NB_PX_Z = 432
+
+#ATTENTION vérifier si les valeurs du json partent à zéro ou à 1
 
 def main() -> None:
     
-    srce = doc.SearchObject('source_arbres')
-    if srce :
-        srce = [child for child in srce.GetChildren()]
-    
-    
     fn = '/Users/olivierdonze/switchdrive/Mandats/Nuit_de_la_science/Programmation_bac_a_sable/tags.json'
+    #pour les arbres sources il faut un objet nommé "source_arbres"
+    #avec en enfant un objet nommé avec le numéro de ID qui contient 
+    #toutes les variantes de l'arbre en enfant (je ne sais pas si je suis clair là !)
+    srce_parent = doc.SearchObject('source_arbres')
+    
+    srce = {int(child.GetName()):child.GetChildren() for child in srce_parent.GetChildren()}
+    
+
+    depthmap = doc.SearchObject('depthmap')
+    mg_depthmap = depthmap.GetMg()
+
     res = c4d.BaseObject(c4d.Onull)
     res.SetName('arbres')
     with open(fn)as f:
         data = json.loads(f.read())
-        for tag in data['tags']:
-            inst = c4d.BaseObject(c4d.Oinstance)
-            inst[c4d.INSTANCEOBJECT_RENDERINSTANCE_MODE] = c4d.INSTANCEOBJECT_RENDERINSTANCE_MODE_SINGLEINSTANCE
-            if srce:
-                inst[c4d.INSTANCEOBJECT_LINK] = random.choice(srce)
-            pos = c4d.Vector(tag['position']['x']*FACT_ECHELLE,0,tag['position']['y']*FACT_ECHELLE)
-            inst.SetAbsPos(pos)
-            angle  = c4d.utils.DegToRad(tag['rotation'])
-            inst.SetAbsRot(c4d.Vector(angle,0,0))
-            inst.SetName(tag['tag_id'])
-            inst.InsertUnderLast(res)
+        
+    for tag in data['tags']:       
+        
+        inst = c4d.BaseObject(c4d.Oinstance)
+        inst[c4d.INSTANCEOBJECT_RENDERINSTANCE_MODE] = c4d.INSTANCEOBJECT_RENDERINSTANCE_MODE_SINGLEINSTANCE
+
+        inst[c4d.INSTANCEOBJECT_LINK] = random.choice(srce[tag['tag_id']])
+
+        #pos = c4d.Vector(tag['position']['x']*FACT_ECHELLE,0,tag['position']['y']*FACT_ECHELLE)
+        
+        #ATTENTION j'ai inversé le x et le y parce qu'il que sur les valeurs test
+        #il y a des valeurs en y de 440 qui est plus grand que 432 ! (à demander à Alexis)
+        #si les valeurs partent de zéro enlever les deux -1
+
+        id_pt = (tag['position']['x'])*NB_PX_X + tag['position']['y']
+        pos = c4d.Vector(depthmap.GetPoint(id_pt)* mg_depthmap)
+
+        inst.SetAbsPos(pos)
+        inst.SetAbsScale(c4d.Vector(FACT_ECHELLE))
+        
+        #angle  = c4d.utils.DegToRad(tag['rotation'])
+        #inst.SetAbsRot(c4d.Vector(angle,0,0))
+        
+        #nom avec 
+        inst.SetName(tag['tag_id'])
+        inst.InsertUnderLast(res)
+        
+        #look at camera tag insertion
+        tag_look = c4d.BaseTag(c4d.Tlookatcamera)
+        inst.InsertTag(tag_look)
     doc.InsertObject(res)
     c4d.EventAdd()
 
